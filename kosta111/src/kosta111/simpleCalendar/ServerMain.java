557de55a4fef9fb523545f727000a72b568527f8
eigen94 <i_b@naver.com>
2016-02-08 23:33:35 +0900

    commit test

--- kosta111/src/kosta111/simpleCalendar/ServerMain.java
+++ kosta111/src/kosta111/simpleCalendar/ServerMain.java
@@ -1,9 +1,21 @@
 package kosta111.simpleCalendar;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
 
 	public static void main(String[] args) {
-		System.out.println("Test!");
-		System.out.println("Test2!");
+		ServerSocket server = null;
+		Socket socket = null;
+		
+		try {
+			server = new ServerSocket(9090);
+			socket = server.accept();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
